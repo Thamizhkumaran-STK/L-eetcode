0685e2289458160dd5f70446b0b3c41c9b8529b3
SthamizhK <thamizhstk07@gmail.com>
2026-06-22 18:14:53 +0530

    Second

---
+++ reverseinteger.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int reverse(int x) {
+        int n=0;
+        while(x!=0)
+        {
+            int a=x%10;
+             if (n > Integer.MAX_VALUE / 10 || 
+                n < Integer.MIN_VALUE / 10) {
+                return 0;
+            }
+            n=(n*10)+a;
+            x/=10;
+        }
+    return n;
+    }
+}
